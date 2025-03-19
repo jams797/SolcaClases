a1d72a4b428079c3c63526385c59ee77156e3051
Jose Moran <amirmosan@gmail.com>
2025-03-19 15:24:25 -0500

    first commit

---
+++ ApiSolcaClase/Models/AppModels/WeatherForecast/WeatherForecastModelReponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiSolcaClase.Models.AppModels.WeatherForecast
+{
+    public class WeatherForecastModelReponse
+    {
+        public WeatherForecastModelReponse(int code, string message)
+        {
+            this.code = code;
+            this.message = message;
+        }
+
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
